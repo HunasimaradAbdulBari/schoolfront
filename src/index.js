a3c9b1b58fe6beb560cf2e6b46069a920f73f074
HunasimaradAbdulBari <sanoof151@gmail.com>
2025-07-24 10:45:29 +0530

    build my app3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
+// import ErrorBoundary from './ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
