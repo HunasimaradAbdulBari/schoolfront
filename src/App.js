e1d8e3862fb154ad28a00f420ae76725cea74dfb
HunasimaradAbdulBari <sanoof151@gmail.com>
2025-07-25 23:13:48 +0530

    dashboard modify-new1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext';
 import Login from './components/Login';
 import Register from './components/Register';
+import AstraLandingPage from './components/AstraLandingPage';
 import Dashboard from './components/Dashboard';
 import Students from './components/Students';
 import Layout from './components/Layout';
@@ -20,7 +21,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/students" replace />} />
+            <Route path="/astralandingpage" element={<AstraLandingPage/>} />
+              <Route index element={<Navigate to="/AstraLandingPage" replace />} />
 
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="students" element={<Students />} />
