58eebdbd9bcac545b1362896674ef892d8f9d2b0
HunasimaradAbdulBari <sanoof151@gmail.com>
2025-09-03 01:00:56 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
-export const API_BASE_URL = "https://schoolback-1.onrender.com";
 
 
 
 
+// src/config.js
+const API_BASE_URL = process.env.REACT_APP_API_URL || 
+  (process.env.NODE_ENV === 'production' 
+    ? 'https://schoolback-1.onrender.com' 
+    : 'http://localhost:10000');
+
+export { API_BASE_URL };
 
